295d6f564a48fd669e78e49f7b0f752be587c622
Julio Vinces <jvi3991zrj@gamil.com>
2024-08-19 17:54:04 -0500

    Aplciacion base de notificaciones para genesis

---
+++ src/public/worker.js
@@ -0,0 +1,10 @@
+console.log('Service Worker')
+
+self.addEventListener('push', e => {
+    const data = e.data.json();
+    console.log(data)
+    self.registration.showNotification(data.title, {
+        body: data.message,
+        icon: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTnKvQE0xOyMLhnfmhBRZbUXkmQWmlTTMGPUABNn7bNs9XYRi1W'
+    })
+})
